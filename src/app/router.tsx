82ca29e6383087e95f4a2ae3b60ec1c31a7f5603
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-06-30 16:59:23 -0400

    setup

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,19 +1,22 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 const Router = () => {
   return (
     <Routes>
-      <Route index /> // landing
-      <Route path="login" />
-      <Route path="register" />
+      <Route index element={<p>Landing</p>} />
+      <Route path="login" element={<p>Login</p>} />
+      <Route path="register" element={<p>Register</p>} />
+      <Route path="app" element={<Navigate to="/channels/@me" />} />
       <Route path="channels">
-        <Route path="@me/:roomId" /> // dm
-        <Route path=":serverId/:roomId" /> // server channel
-        <Route path="@me" /> // dashboard
-        <Route index /> // dashboard
+        <Route path="@me">
+          <Route path=":roomId" element={<p>DM room</p>} />
+          <Route index element={<p>Dashboard</p>} />
+        </Route>
+        <Route path=":serverId/:roomId" element={<p>Server channel room</p>} />
+        <Route index element={<Navigate to="/channels/@me" />} />
       </Route>
-      <Route path="guild-discovery" /> // discover servers
-      <Route path="*" /> // 404
+      <Route path="guild-discovery" element={<p>Discover servers</p>} />
+      <Route path="*" element={<p>404 not found</p>} />
     </Routes>
   );
 };
