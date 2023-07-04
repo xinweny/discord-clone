a88ea018077e067981190e2526f041176b681b4d
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-07-04 18:39:33 -0400

    add export and type

--- src/common/components/ui/button.component.tsx
+++ src/common/components/ui/button.component.tsx
@@ -3,12 +3,15 @@ import React, { ButtonHTMLAttributes } from 'react';
 export interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
+  type?: 'button' | 'submit' | 'reset';
 }
 
-export const Button = ({ children, onClick }: IButton) => {
+export const Button = (
+  { children, onClick, type = 'submit' }: IButton
+) => {
   return (
-    <button onClick={onClick}>
+    <button onClick={onClick} type={type}>
       {children}
     </button>
-  )
+  );
 };
