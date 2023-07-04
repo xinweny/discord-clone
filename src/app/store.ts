c8de5ba2d93e9f24d7d4190e12a83fbd1ff23d6f
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-07-04 18:34:16 -0400

    set type naming convention

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,6 +4,6 @@ const store = configureStore({
   reducer: {},
 });
 
-export type RootState = ReturnType<typeof store.getState>;
+export type IRootState = ReturnType<typeof store.getState>;
 
 export { store };
