69ec9d9cdb94f34c0f41e564c2d97d46b5f861fe
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-09-07 16:23:18 -0400

    add deep populate of user's dms

--- src/api/users/dms/service.ts
+++ src/api/users/dms/service.ts
@@ -5,7 +5,13 @@ import { User } from '../model';
 const getDms = async (userId: Types.ObjectId | string) => {
   const user = await User.findById(userId, 'dmIds')
     .populate([
-      { path: 'dms' },
+      {
+        path: 'dms',
+        populate: {
+          path: 'participants',
+          select: 'displayName username avatarUrl'
+        },
+      },
     ]);
 
   if (!user) return null;
