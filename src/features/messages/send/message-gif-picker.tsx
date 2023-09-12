5dd8fa2e763f363195fc948045b4ae3a04c7a7da
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-09-12 12:20:02 -0400

    add react-intl

--- src/features/messages/send/message-gif-picker.tsx
+++ src/features/messages/send/message-gif-picker.tsx
@@ -1,16 +1,22 @@
 import GifPicker, { type TenorImage } from 'gif-picker-react';
+import { useIntl } from 'react-intl';
 
 import { env } from '@config';
 
 export function MessageGifPicker() {
+  const intl = useIntl();
+
   const handleClick = (gif: TenorImage) => {
     console.log(gif);
   };
+
+  console.log(env.VITE_TENOR_API_KEY);
 
   return (
     <GifPicker
       tenorApiKey={env.VITE_TENOR_API_KEY}
       onGifClick={handleClick}
+      locale={intl.locale}
     />
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { IntlProvider } from 'react-intl';
 
 import App, { store } from '@app';
 
@@ -11,10 +12,12 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <IntlProvider locale={navigator.language}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </IntlProvider>
   </React.StrictMode>,
 );
