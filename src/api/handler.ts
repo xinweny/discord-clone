1b52f07987a0ea31e59a6862abb9c45aa9af1142
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-09-20 16:43:48 -0400

    add join handler

--- src/api/handler.ts
+++ src/api/handler.ts
@@ -6,6 +6,10 @@ export const connectionHandler = async (socket: Socket) => {
   await connectionService.set(socket);
   console.log(`${socket.id} connected`);
 
+  socket.on('join', async (roomName: string | string[]) => {
+    socket.join(roomName);
+  });
+
   socket.on('disconnect', async () => {
     await connectionService.remove(socket.user._id);
     console.log(`${socket.id} disconnected`);
