2dd0333555055bf4d4e9f4247208fe97ca4b483f
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-10-26 14:21:43 -0400

    resolve alias for scss

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +9,9 @@ export default defineConfig({
   define: {
     'process.env': loadEnv(process.env.NODE_ENV, process.cwd(), ''),
   },
-})
+  resolve: {
+    alias: {
+      '@assets': path.join(__dirname, './src/assets'),
+    },
+  },
+});
