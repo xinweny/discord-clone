ff335f193deafba55e8c67db9fa1d3d6080d1619
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-10-26 18:11:45 -0400

    add svg support

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'path';
 
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    svgr({
+      include: '**/*.svg',
+    }),
+    tsconfigPaths(),
+  ],
   define: {
     'process.env': loadEnv(process.env.NODE_ENV, process.cwd(), ''),
   },
