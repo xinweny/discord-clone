db42fc9df06af533eaacfb0c068f608146a5e34d
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-01-15 16:34:56 -0500

    finish styling overview form

--- src/features/servers/edit/server-banner-input.tsx
+++ src/features/servers/edit/server-banner-input.tsx
@@ -4,6 +4,10 @@ import { ImagePreview } from '@components/ui/media';
 
 import { FormGroup, FileInput } from '@components/ui/forms';
 
+import styles from './server-banner-input.module.scss';
+
+import ImageUploaderIcon from '@assets/icons/image-uploader.svg?react';
+
 type ServerBannerInputProps = {
   src?: string;
 };
@@ -17,43 +21,52 @@ export function ServerBannerInput({
   const banner = useWatch({ control, name })
 
   return (
-    <div>
-      <FormGroup label="Server Banner Background" htmlFor="server-banner">
-        <div>
-          <p>This image will display at the top of your channels list.</p>
-          <p>The recommended minimum size is 960x540 and recommended aspect ratio is 16:9.</p>
+    <FormGroup
+      label="Server Banner Background"
+      htmlFor="server-banner"
+    >
+      <div className={styles.container}>
+        <div className={styles.description}>
+          <span>This image will display at the top of your channels list.</span>
+          <span>The recommended minimum size is 960x540 and recommended aspect ratio is 16:9.</span>
+          <label htmlFor="server-banner">
+            <div role="button">Upload Background</div>
+            <FileInput
+              id="server-banner"
+              name={name}
+              accept="image/*"
+              label="Upload Banner"
+              hidden
+            />
+          </label>
         </div>
-        <label htmlFor="upload-image">
-          <div>Upload Background</div>
-          <FileInput
-            id="server-banner"
-            name={name}
-            accept="image/*"
-            label="Upload Banner"
-            hidden
-          />
-        </label>
-      </FormGroup>
-      <div>
-        <label htmlFor="server-banner">
-          <ImagePreview name={name}>
-            <img src={src} alt="" />
-          </ImagePreview>
-          <FileInput
-            id="server-banner"
-            name={name}
-            accept="image/*"
-            label="Upload Banner"
-            hidden
-          />
-        </label>
-        {banner && (
-          <button
-            type="button"
-            onClick={() => { setValue(name, undefined); }}
-          >Remove</button>
-        )}
+        <div className={styles.input}>
+          <label htmlFor="server-banner">
+            <div className={styles.iconWrapper}>
+              <ImagePreview name={name} className={styles.preview}>
+                {src
+                  ? <img src={src} alt="Preview" />
+                  : <div></div>
+                }
+              </ImagePreview>
+              <div className={styles.icon}><ImageUploaderIcon /></div>
+            </div>
+            <FileInput
+              id="server-banner"
+              name={name}
+              accept="image/*"
+              label="Upload Banner"
+              hidden
+            />
+          </label>
+          {banner && (
+            <button
+              type="button"
+              onClick={() => { setValue(name, undefined); }}
+            >Remove</button>
+          )}
+        </div>
       </div>
-    </div>
+    </FormGroup>
   );
 }
