3a8b9622b26d143d5fc91e159e9de971ad7c964a
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-01-25 14:11:22 -0500

    increase req rate limit

--- src/config/rateLimit.ts
+++ src/config/rateLimit.ts
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 export const apiRateLimiter = rateLimit({
   windowMs: 60000,
-  max: 100,
+  max: 10000,
   standardHeaders: true,
   legacyHeaders: false,
 });
