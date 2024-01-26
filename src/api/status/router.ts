fc1df68e4f49f8ce1f6e8670503ba7f5d55b362b
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-01-26 13:58:00 -0500

    add multiple getusers status

--- src/api/status/router.ts
+++ src/api/status/router.ts
@@ -4,6 +4,8 @@ import { statusController } from './controller';
 
 const statusRouter = Router({ mergeParams: true });
 
+statusRouter.get('/users/', statusController.getUserStatuses);
+
 statusRouter.get('/users/:userId', statusController.getUserStatus);
 
 export { statusRouter };
