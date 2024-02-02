ff4b7362f36874b8bf9dd493c434130a77b766ad
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-02-02 15:09:08 -0500

    remove unused code

--- backend/src/config/livekit.ts
+++ backend/src/config/livekit.ts
@@ -5,7 +5,7 @@ import env from '@config/env';
 export const livekitClient = new RoomServiceClient(
   env.LK_URL,
   env.LK_API_KEY,
-  env.LK_API_SECRET
+  env.LK_API_SECRET,
 );
 
 export const receiver = new WebhookReceiver(env.LK_API_KEY, env.LK_API_SECRET);
