924540e8e37b9d0626977ba0dcde2da95d1d69f9
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-02-15 13:56:24 -0500

    add login to demo account button

--- frontend/src/features/landing/demo-button.tsx
+++ frontend/src/features/landing/demo-button.tsx
@@ -1,14 +1,25 @@
-import { useOutletContext } from 'react-router-dom';
+import { useNavigate, useOutletContext } from 'react-router-dom';
 
 import type { LoggedInContext } from '@components/routes';
+
+import { useLoginMutation } from '@features/auth/api';
 
 export function DemoButton({ className }: React.HTMLAttributes<HTMLButtonElement>) {
   const { isLoggedIn } = useOutletContext<LoggedInContext>();
 
+  const [login] = useLoginMutation();
+
+  const navigate = useNavigate();
+
   if (isLoggedIn) return null;
 
-  const loginDemoAccount = () => {
-    console.log('TODO: Login Demo account and redirect to dashboard');
+  const loginDemoAccount = async () => {
+    await login({
+      email: 'byteborn2@jigsy.com',
+      password: 'password2',
+    });
+
+    navigate('/channels/@me');
   };
 
   return (
