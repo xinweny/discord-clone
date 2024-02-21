a615059c8c0f5b4e41c535dcc388544831be0b42
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-02-21 16:04:24 -0500

    add meta tag for verification

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Helmet } from 'react-helmet';
 
 import App from '@app';
 
@@ -10,6 +11,9 @@ import './assets/styles/reset.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <Helmet>
+      <meta name="google-site-verification" content="LKdObr0tK_XMXBddJwBLhJKj1VUM9V71ajJ2hHJ6Jhw" />
+    </Helmet>
     <App />
   </React.StrictMode>,
 );
