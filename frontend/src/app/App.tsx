2c5322f7bff273b2410154eeaab7a5e81fccb573
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-02-21 16:21:34 -0500

    add verification meta tag directly to html

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,10 +1,8 @@
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { IntlProvider } from 'react-intl';
-import { HelmetProvider } from 'react-helmet-async';
 
 import { store } from './store';
-import { SEO } from './seo';
 
 import Router from './router';
 
@@ -18,20 +16,17 @@ import 'react-resizable/css/styles.css';
 import styles from './app.module.scss';
 
 const App = () => (
-  <HelmetProvider context={{}}>
-    <SEO />
-    <div id="app-root" className={styles.app}>
-      <SvgMasks />
-      <IntlProvider locale={navigator.language}>
-        <Provider store={store}>
-          <BrowserRouter>
-            <Router />
-          </BrowserRouter>
-        </Provider>
-      </IntlProvider>
-      <Portals numLayers={5} />
-    </div>
-  </HelmetProvider>
+  <div id="app-root" className={styles.app}>
+    <SvgMasks />
+    <IntlProvider locale={navigator.language}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </Provider>
+    </IntlProvider>
+    <Portals numLayers={5} />
+  </div>
 );
 
 export default App;
