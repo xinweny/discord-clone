6175d672ff8464fcc8039656511130e87b151840
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-02-22 12:49:25 -0500

    add disclaimer

--- frontend/src/features/landing/footer.tsx
+++ frontend/src/features/landing/footer.tsx
@@ -2,9 +2,9 @@ import { LinkImage } from '@components/ui/links';
 
 import { LandingLogo } from './landing-logo';
 
-import styles from './footer.module.scss';
-
 import GithubIcon from '@assets/icons/github.svg?react';
+
+import styles from './footer.module.scss';
 
 export function Footer() {
   const ghUrl = 'https://github.com/xinweny';
@@ -14,12 +14,13 @@ export function Footer() {
       <div>
         <LandingLogo />
         <div className={styles.content}>
-          <p>Made by <strong><a href={ghUrl}>xinweny</a></strong> in 2023</p>
+          <p>Made by <a href={ghUrl} className={styles.githubLink}>xinweny</a> in 2023</p>
           <LinkImage href={`${ghUrl}/discord-clone-frontend`}>
             <GithubIcon />
           </LinkImage>
         </div>
       </div>
+      <p className={styles.disclaimer}><span>DISCLAIMER:</span> This personal project is made solely for educational purposes with the intent to demonstrate my skills as a Full Stack JS Developer. This project has no affiliation with Discord Inc. or any of their related and/or associated companies. I do not not make any warranty or representation, and disclaim all liabilities, including but not limited to the loss of data, that may arise from the use of this project.</p>
     </footer>
   );
 }
